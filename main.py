3fc5b9c72d1ac96dd88b72c639f3ab88afcd2bcc
zacvega <zacvega@hotmail.com>
2024-10-03 10:43:32 -0500

    Added classes

---
+++ main.py
@@ -0,0 +1,20 @@
+class Board:
+    def __init__(self):
+        self.board = [5][6]
+
+
+
+
+class Player:
+    def __init__(self, FirstMove, LookAheadDepth):
+        self.FirstMove = FirstMove
+        self.LookAheadDepth = LookAheadDepth
+    
+            
+
+
+def main():
+    return 0
+
+if(__name__ == "__main__"):
+    main()
