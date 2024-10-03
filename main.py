8843985a8f72557bd18cf8266b97e25eeea68b1a
zacvega <zacvega@hotmail.com>
2024-10-03 12:04:46 -0500

    adding functions

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ class Board:
     def __init__(self):
         self.board = [5][6]
 
+    # def OpenSpaces(self):
 
+    # def CheckForWinner(self):
 
+    # def PlacePiece(self):
 
 class Player:
     def __init__(self, FirstMove, LookAheadDepth):
@@ -12,9 +15,16 @@ class Player:
     
             
 def PlayGame(board, p1, p2):
+    while(1):
+        board.p
     return 0
 
 def main():
+    board = Board()
+    player1 = Player((3,4),2)
+    player2 = Player((3,3),4)
+    PlayGame(board, player1, player2)
+
     return 0
 
 if(__name__ == "__main__"):
