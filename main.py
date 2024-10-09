02a8ca44a8865c19b324958f99d00f89594c30fa
Ando Pepe <coolcatando@gmail.com>
2024-10-09 14:13:50 -0500

    board class

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
-# class Board:
-#     def __init__(self):
-#         self.board = [5][6]
+class Board:
+    def __init__(self, xSquares = [], oSquares = [], rows = 5, columns = 6):
+        """
+        Initializes the space with a vacuum and dirty rooms
 
-#     def placeableSpaces(self):
+        ## Parameters
+        * rows:  Number of rows for space 
+            * Type: int
 
-#     def CheckForWinner(self):
+        * columns: Number of columns for space 
+            * Type: int
+         * oSquares: List of location(s) (ordered pairs) for squares claimed by no one 
+            * Type: [(int, int),...]
+            * (1 based index)
+        * oSquares: List of location(s) (ordered pairs) for squares claimed by player o 
+            * Type: [(int, int),...]
+            * (1 based index)
+        * xSquares: List of location(s) (ordered pairs) for squares claimed by player x 
+            * Type: [(int, int),...]
+            * (1 based index)
+        """
+        
+        self.eSquares = list()
+        for i in range(rows):
+            for j in range(columns):
+                if((i+1,j+1) not in oSquares and (i+1,j+1) not in xSquares):
+                    self.eSquares.append((i+1, j+1))
+                
+        self.oSquares = oSquares
+        self.xSquares = oSquares
 
+    def PlacePiece(self, owner, location):
+        self.eSquares.remove(location)
+        if (owner == 'o'):
+            self.oSquares.append(location)
+        elif (owner == 'x'):
+            self.xSquares.append(location)
+
+    def CheckForWinner(self):
+        return True
+
+    # not done yet currently returns empty squares
+    def placableSpaces(self):
+        return self.eSquares
+    
+    # def CheckForWinner(self): 
 
 class Player:
     def __init__(self, FirstMove, LookAheadDepth):
@@ -68,9 +106,12 @@ def PlayGame(board, p1, p2):
 
 def main():
     board = Board()
-    player1 = Player((3,4),2)
-    player2 = Player((3,3),4)
-    PlayGame(board, player1, player2)
+    # print(board.placableSpaces())
+    # print(board.placableSpaces())
+
+    # player1 = Player((3,4),2)
+    # player2 = Player((3,3),4)
+    # PlayGame(board, player1, player2)
 
     return 0
 
